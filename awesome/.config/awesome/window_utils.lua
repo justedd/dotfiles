3a607e69de4f07a1f7236beb1278d3bb95f3779b
justed <dev.justed@gmail.com>
2021-08-19 01:24:19 +0500

    really keep mouse positoin

--- awesome/.config/awesome/window_utils.lua
+++ awesome/.config/awesome/window_utils.lua
@@ -1,10 +1,12 @@
 local awful = require('awful')
 
+local naughty = require("naughty")
+
 local obj = {}
 
 -- TODO remember previously focused client
 
-function focus(screen, mouselocation_x, mouselocation_y)
+function obj.focus(screen, mouselocation_x, mouselocation_y)
   awful.screen.focus(screen)
 
   local client = screen.clients[1]
@@ -20,7 +22,7 @@ function focus(screen, mouselocation_x, mouselocation_y)
   }
 end
 
-function get_active()
+function obj.get_active()
   return awful.screen.focused()
 end
 
@@ -32,33 +34,36 @@ function obj.previous_workspace(keep_focus)
     local mouselocation_y = mouse.coords().y
   end
 
-  local active_screen = get_active()
+  local active_screen = obj.get_active()
 
   for i = 1, screen.count() do
     awful.tag.viewprev(screen[i])
   end
 
   if keep_focus then
-    focus(active_screen, mouselocation_x, mouselocation_y)
+    obj.focus(active_screen, mouselocation_x, mouselocation_y)
   end
 end
 
 function obj.next_workspace(keep_focus)
   keep_focus = keep_focus or false
 
+  local mouselocation_x
+  local mouselocation_y
+
   if keep_focus then
-    local mouselocation_x = mouse.coords().x
-    local mouselocation_y = mouse.coords().y
+    mouselocation_x = mouse.coords().x
+    mouselocation_y = mouse.coords().y
   end
 
-  local active_screen = get_active()
+  local active_screen = obj.get_active()
 
   for i = 1, screen.count() do
     awful.tag.viewnext(screen[i])
   end
 
   if keep_focus then
-    focus(active_screen, mouselocation_x, mouselocation_y)
+    obj.focus(active_screen, mouselocation_x, mouselocation_y)
   end
 end
 
