38a54af3a85a96185027d63b2d13f2cdd3adbb29
justed <dev.justed@gmail.com>
2023-06-06 19:23:49 +0500

    [awesome] Try to use focus colors again

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -415,8 +415,8 @@ client.connect_signal("manage", function (c)
     end
 end)
 
---client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
---client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
 -- Enable sloppy focus, so that focus follows mouse.
