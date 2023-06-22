e12cac984d877624ebea3b7c980a096f3f550c8f
justed <dev.justed@gmail.com>
2023-06-22 22:32:58 +0500

    [awesome] Extract error recovery

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -28,25 +28,8 @@ local tiny_volume = require('widgets/tiny_volume')
 
 local window_utils = require('window_utils')
 
-if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
-end
+dofile(awful.util.getdir("config") .. "/" .. 'load_recovery.lua')
 
-do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
-
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
-        in_error = false
-    end)
-end
 
 beautiful.init("~/.config/awesome/theme.lua")
 
