bb719711d20080f5788e5cefe3d7a797cd303c06
justed <dev.justed@gmail.com>
2024-02-11 17:05:41 +0500

    [nvim] Switch to shopify lsp

--- nvim/.config/nvim/lua/justed.lua
+++ nvim/.config/nvim/lua/justed.lua
@@ -112,8 +112,7 @@ cmp.setup({
   })
 })
 
--- TODO: understand how to not show the diagnostics
-require'lspconfig'.solargraph.setup{
+require'lspconfig'.ruby_ls.setup{
   handlers = {
     ["textDocument/publishDiagnostics"] = vim.lsp.with(
       vim.lsp.diagnostic.on_publish_diagnostics, {
