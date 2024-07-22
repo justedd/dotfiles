a1542c9b30762a0c593450400138430f416a6188
justed <dev.justed@gmail.com>
2024-07-22 22:25:53 +0500

    [awesome] codestyle

--- awesome/.config/awesome/mouse_keys.lua
+++ awesome/.config/awesome/mouse_keys.lua
@@ -3,24 +3,24 @@ local awful = require('awful')
 local window_utils = require('window_utils')
 
 return gears.table.join(
-  awful.button(
-    {}, 3,
-    function()
-      if 1440 - mouse.coords().y > 100 then
-        return
-      end
+    awful.button(
+        {}, 3,
+        function()
+            if 1440 - mouse.coords().y > 100 then
+                return
+            end
 
-      window_utils.previous_workspace(true)
-    end
-  ),
-  awful.button(
-    {}, 1,
-    function()
-      if 1440 - mouse.coords().y > 100 then
-        return
-      end
+            window_utils.previous_workspace(true)
+        end
+    ),
+    awful.button(
+        {}, 1,
+        function()
+            if 1440 - mouse.coords().y > 100 then
+                return
+            end
 
-      window_utils.next_workspace(true)
-    end
-  )
+            window_utils.next_workspace(true)
+        end
+    )
 )
