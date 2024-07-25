5343c14aa039f02c91eca7252baf082d1582c04d
justed <dev.justed@gmail.com>
2024-07-25 22:37:45 +0500

    [awesome] codestyle

--- awesome/.config/awesome/debug_msg.lua
+++ awesome/.config/awesome/debug_msg.lua
@@ -1,5 +1,5 @@
 local naughty = require("naughty")
 
 return function(msg)
-  naughty.notify({ title = msg, message = msg, timeout = 5 })
+    naughty.notify({ title = msg, message = msg, timeout = 5 })
 end
